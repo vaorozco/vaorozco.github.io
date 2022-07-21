7956868d8567e1cb75836df3a5a0028f7cc62719
Víctor Alonso Orozco Calderón <vaorozco@hotmail.com>
2022-07-20 19:07:07 -0600

    test

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 alert('Welcome to my portfolio! Responsive design in progress');
+function openNav(){
+    document.getElementById("sideNav").style.width = "50%";
+}
+
+function closeNav(){
+    document.getElementById("sideNav").style.width = "0%";
+}
+
+
