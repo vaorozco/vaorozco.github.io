791d138e5921d35430d365b568a14e6be3529f8d
Víctor Alonso Orozco Calderón <vaorozco@hotmail.com>
2022-07-22 14:11:58 -0600

    test with js

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ function closeNav(){
 }
 
 
+function submitForm() {
+        
+    const form = document.querySelector('.my-form')
+    const formData = new FormData(form)
+    const url = 'https://formsubmit.co/825c3f0ee6edf063cc01f5ea2ad854a4'
+    fetch(
+      url,
+      {
+        method: 'POST',
+        body: formData
+      }
+    )
+    return false
+}
