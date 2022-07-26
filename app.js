c447b07c3d4a4ca81655b90c630fa33bdade400f
Víctor Alonso Orozco Calderón <vaorozco@hotmail.com>
2022-07-25 19:36:52 -0600

    update

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
-function menuButton(){
-  const menu = document.getElementById("sideNav");
-   if (window.getComputedStyle(menu).display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
+function menuButton() {
+  var menu = document.querySelector(".sideNav");
+  if (menu.style.visiblity = "hidden") {
+    menu.classList.toggle('show-sideNav');
   }
 }
 
 function submitForm() {
-    const form = document.getElementById('form')
-    const formData = new FormData(form)
-    const url = 'https://formsubmit.co/825c3f0ee6edf063cc01f5ea2ad854a4'
-    fetch(
-      url,
-      {
-        method: 'POST',
-        body: formData
-      }
-    )
-    form.reset();
-    showMessage();
-    return false
+  const form = document.getElementById('form')
+  const formData = new FormData(form)
+  const url = 'https://formsubmit.co/825c3f0ee6edf063cc01f5ea2ad854a4'
+  fetch(
+    url,
+    {
+      method: 'POST',
+      body: formData
+    }
+  )
+  form.reset();
+  showMessage();
+  return false
 }
 
 function showMessage() {
-  var visible = document.querySelector(".contact-message");
-  visible.classList.add("show-message");
-  setTimeout(function(){ visible.classList.add("hide-message")}, 3000);
-
+  var message = document.querySelector(".contact-message");
+  message.classList.add("show-message");
+  setTimeout(function () { message.classList.add("hide-message") }, 3000);
 }
-
-
